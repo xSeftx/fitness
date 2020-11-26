c5b630ef7a6dc715cae34cb19c5c3ce273348f6e
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-27 01:34:47 +0400

    fix

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,41 +1,47 @@
 // бургер меню
 const burgerMenu = () => {      
     
-    const menuButton = document.querySelector('.menu-button'),
-        popupMenu = document.querySelector('.popup-menu');  
         
-    menuButton.addEventListener('click', (e) => {
-        let target = e.target.parentNode;        
-        if(target.matches('.menu-button')){
-            popupMenu.style.display = 'block'
-        }
-    })
+        const menuButton = document.querySelector('.menu-button'),
+            popupMenu = document.querySelector('.popup-menu');  
+            
+        menuButton.addEventListener('click', (e) => {
+            let target = e.target.parentNode;        
+            if(target.matches('.menu-button')){
+                popupMenu.style.display = 'block'
+            }
+        })
         
 
-    popupMenu.addEventListener('click', (event) => {        
-        let target = event.target;  
-        console.log(target);      
-        if(target.closest('.close-menu-btn') || target.closest('a')){
-        popupMenu.style.display = 'none'
-        }          
-    })  
+        popupMenu.addEventListener('click', (event) => {        
+            let target = event.target;  
+            console.log(target);      
+            if(target.closest('.close-menu-btn') || target.closest('a')){
+            popupMenu.style.display = 'none'
+            }          
+        });
+
+        
+            
+            if(document.documentElement.offsetWidth <= 768) {  
+                const menu = document.querySelector('.top-menu'),
+                    gift = document.querySelector('.fixed-gift');                 
+                window.addEventListener('scroll', () => { 
+                        if(window.scrollY >= 200){            
+                        menu.style.position = 'fixed';            
+                        gift.style.display = 'none'; 
+                        
+                    }else if(window.scrollY <= 400){
+                        menu.style.position = 'relative';
+                        gift.style.display = 'block';
+                    }                
+                })
+
+            }else {
+                menu.style.position = 'relative';
+            }
+        
     
-    window.addEventListener('scroll', () => {        
-        const menu = document.querySelector('.top-menu'),
-            gift = document.querySelector('.fixed-gift');
-
-        if(window.scrollY >= 200){            
-            menu.style.position = 'fixed';            
-            gift.style.display = 'none';
-            
-            
-        }else if(window.scrollY <= 400){
-            menu.style.position = 'relative';
-            gift.style.display = 'block';
-        }         
-        
-        
-    })
 }
 
 export default burgerMenu
