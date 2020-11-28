7cdcf2f04e9b2a912bd8a58c55cafb3b98bbac9e
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-11-28 13:41:02 +0400

    add calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,13 +1,52 @@
 
 // калькулятор
-const calc = () => {
-    const priceMessage = document.querySelector('.price-massage>input');
-    console.log(priceMessage);
+const calc = () => {    
+    const formCalc = document.querySelector('#cards .right'),
+    timeInputs = formCalc.querySelectorAll('input[name="card-type"]'),
+    priceTotal = formCalc.querySelector('#price-total'),
+    promoInput = formCalc.querySelector('.price-message input'),
+    promoCode = 'ТЕЛО2019';
 
+    const cost = {
+    mozaika: [1999, 9900, 13900, 19900],
+    schelkovo: [2999, 14990, 21990, 24990]
+    };
 
+    let currentCost = cost.mozaika;
+    priceTotal.textContent = currentCost[0];
 
+    const totalOutput = () => {
+        timeInputs.forEach((item, i) => {
+            if (item.checked) {
+                if (promoInput.value === promoCode) {
+                priceTotal.textContent = Math.ceil(currentCost[i] * 0.7);
+                } else {
+                priceTotal.textContent = currentCost[i];
+                }
+            }
+        });
+    };
+
+    promoInput.addEventListener('input', () => {
+        totalOutput();
+    });
+
+    formCalc.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target.matches('input[name="club-name"]')) {
+            currentCost = cost[target.value];
+            totalOutput();
+        }
+
+        if (target.matches('input[name="card-type"]')) {
+            timeInputs.forEach((item) => {
+                item.removeAttribute('checked');
+                if (target === item) {
+                target.setAttribute('checked', '');
+                totalOutput();
+                }
+            });
+        }
+    });
 }
-
-
-
-export default calc;
+export default calc
--- src/modules/gift.js
+++ src/modules/gift.js
@@ -1,26 +1,27 @@
 // подарок
 const gift = () => {
 
-    const gift = document.querySelector('.fixed-gift'),
-        popupGift = document.getElementById('gift');
-    
-    gift.addEventListener('click', () => {        
-        popupGift.style.display = 'block';             
-        gift.style.display = 'none';
-    });        
-    
-    popupGift.addEventListener('click', (event) => {
-        let target = event.target;        
-        if(target.matches('.close_icon, .close-btn')){            
-            popupGift.style.display = 'none';
-        } else {
-            target = target.closest('.form-wrapper');
-            if(!target){
-                popupGift.style.display = 'none';                
-            }
-        }
-        
-    });
-}
+    const body = document.querySelector('body'),
+        giftBtn = document.querySelector('.fixed-gift'),
+        giftPopup = document.querySelector('#gift');
+
+  if (giftPopup === null) {
+    return;
+  }
+  body.addEventListener('click', (e) => {
+    let target = e.target,
+        targetClos = target.closest('.fixed-gift');
+    if (targetClos === giftBtn) {
+      console.log(giftPopup);
+      giftPopup.style.display = 'block';
+      giftBtn.remove();
+    }
+    if (giftPopup.style.display === 'block' && (target.classList.contains('overlay', 'close_icon', 'close-btn') || target.classList.contains('close_icon') || target.classList.contains('close-btn'))) {
+      giftPopup.style.display = 'none';
+    }
+  });
+
+};
+
 
 export default gift
